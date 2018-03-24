da7f528afa8bcb9fded7ef10ecd8fd64c863a133
nathabhaibhutiya <31032441+nathabhaibhutiya@users.noreply.github.com>
2018-03-24 14:33:02 +0530

    Add files via upload

---
+++ application/downloadform.php
@@ -0,0 +1,146 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="application.css">
+</head>
+<body>
+    <div class="main">
+       <div class="header">
+         <div class="section1">
+          <div class="image1">
+             <img src="images/img1.png">
+          </div>
+
+          <div class="heading">
+            <h1 style="color:red; text-decoration: underline;">Wizard Bricks</h1>
+            <span style=" text-decoration: underline;font-style: italic; font-size:22px;font-weight: bold;">.....where dreams turn real</span><br>
+            <span style="font-size:22px;">ISO: 9001-2008 CERTIFIED</span>
+          </div>
+          <div class="image2">
+             <img src="images/img2.png" width="145" height="115">
+          </div>
+          
+          <div class="image3">
+             <img src="images/img3.png" width="125" height="115">
+          </div>
+       </div>
+       <hr style="width: 93%; background-color: red; height: 5px;margin:0px auto;">
+
+
+       <div class="section2">
+
+          <p> UNIT NO. 208, 2nd Floor, ILD Trade Center, Sohna Road, Sector-47,<br>
+              Gurgaon, Haryana - 122018 (INDIA). Ph: 0124 - 4031069</p><br>
+              <span style="color:black;font-weight: bold;">E-mail: </span><a href="#">info@wizardbricks.com</a><br>
+               <span style="color:black;font-weight: bold;">Web Site:</span><a href="#"> www.wizardbricks.com</a>
+        </div>
+
+
+        <div class="section3">
+
+       <h4 style="text-align: center;color: #000;margin: 50px 0px 0px 30px;font-size: 18px;">SUBJECT: - APPLICATION FORM</h4>
+       <div style="font-size: 18px;color: #000;padding-top: 10px;">Dear Sir/Madam</div>
+        	<p style="margin-top: 30px;margin-left: 90px;color: #000;font-size: 18px;font-weight: bold;">First Applicant</p>
+        	 <div class="box">
+        	   <p>AFFIX YOUR <br>PASSPORT <br>SIZE PHOTO<br> GRAPH<br> HERE</p>
+             </div>
+
+             <div class="form">
+                         <label>
+                            <span>Name</span><input type="text" >
+                         </label>
+                         
+                         <label>
+                              <span>Mailing Address</span><input type="text">
+                         </label>
+                         <label>
+                              <span>Permanent Address</span><input type="text">
+                         </label>
+                         <label>
+                              <span>Phone Number</span><input type="text">
+                         </label>
+                         <label>
+                         <span>Permanent A/C No.</span><input type="text">
+                         </label>
+                         <label>
+                            <span>Date of Birth</span><input type="text">
+                         </label>
+                         <label>
+                            <span>E-mail ID</span><input type="text">
+                          </label>
+                    
+              </div>
+
+              <div class="Tentative">
+                 <h3>Tentative details of the Unit/Units:-</h3>
+                      <p style="margin-left: 40px;margin-top: 5px;font-size: 18px;margin-bottom: 50px;">1. Unit/Units admeasuring area of __________________ sq.yd.</p>
+
+                   <h3>UNDERTAKING</h3>
+
+                   <p>That I/We Applicant(s) do hereby declare that my/our request for allotment is irrevocable and that the above particulars /information given by me are true and correct and nothing has beenmisrepresented / concealed there from. I/We undertake to inform the company of any changein the above particulars / information particularly the address, till the property, if allotted, is registered in my / our name(s).
+    <br>
+I/We request that I /We may be registered for allotment of a unit, in upcoming projects of the
+company for which I/We pay herewith a sum of Rs.________________________
+(Rupees_________________________________________________________ only) by thebank Draft / Cheque No. __________________ dated for ____________________ drawn on
+____________________________________ in favour of wizardbricks as advance for registration amount which is paid subject to the following conditions.<br><br><br>
+1) It is understood that company shall allot me/us the Unit at Basic Price of
+__________________ per sq. yd.<br><br><br>
+2) I/We agree that if there should be any variation in the area of the Unit at the time of
+booking / allotment then the payment for the excess/ shortfall in area be paid / refunded, as
+the case may be proportionately at the same rate as agreed herein.<br><br><br>
+3) That I/We have clearly understood that this application for registration does not constitute
+an Agreement and I/We do not become entitled to allotment by virtue of this application.<br><br><br>
+4) I/We also understand and agree that on payment of 30% of the total Basic Sale Price by
+me/us, the Developers /buyer agreement will be executed.<br><br><br>
+5) This application is valid only subject to clearance of the Cheque.<p>
+
+
+
+<h3>In addition I/We agree that:</h3>
+
+<p>(i) EDC and any such charges/Taxes by the Govt. or any of its bodies constituted by it and IDC / PLC(s) as determined by the Company shall be charged extra.<br><br><br>
+(ii) Charges like Maintenance / Power Back up / Parking / STP / External Electrification and such other charges as may be determined by the company for all allottees shall be charged extra at the time of possession or earlier.<br><br><br>
+(iii) Subleasing is permissible with the Consent of the Company or on being authorized by it,in the case of built up property.<br><br><br>
+(iv) In the event of default in making payment of any installment(s)/amounts as demanded by the company or any of the other terms and conditions agreed herein, my/our registration shall be cancelled and I/We shall be left with no right, lien or interest therein except claim of
+refund of the amount paid by me /us after deduction of 40% of the amount deposited without Penalty with in 5(five) months from the date of cancellation.<br><br><br>
+(v) That for cancellation of the booking at my/our behest/request, I/We shall be left with no right, lien or interest save and except the claim of 60% refund of the amount of booking paid by me / us, without Penalty, in 5 (five) months from the date of acceptance of cancellation.<br><br><br>
+(vi) I/We understand & agree that finalization of allotment shall be subject to due execution of the Developer /Buyer Agreement.<br><br><br>
+(vii) That I/We understand and agree that in case the Buyer fails to execute Developer/ Buyer Agreement, his booking will be cancelled leading to forfeiture of 40% of the amount paid.<br><br><br>
+(viii) That in future for surrender / cancellation of the registration I/We shall give NOC (No Objection Certificate) from the dealer through whom booking is/ was made.<br><br><br>
+(ix) I/We understand & agree that if the installment(s) or any other demands as & when raised by the company are not paid in due time, the buyer(s) will be liable to pay interest as per the company’s policy.<br><br><br>
+(x) That the advance paid by me/us would be adjusted against the amount payable for the allotment of the unit.<br><br><br>
+(xi) That I/We understand and agree that size of unit will be subject to variation of 15-20%.<br><br><br>
+(xii) That the company reserves its right to reject my/our application and cancel my/our registration without assigning any reason there of and in such event I shall only be entitled to get the amount of advance paid by me along with simple Penalty @ 8% per annum from the date of payment in 5(five) months from the date of cancellation.<br><br><br>
+(xii) That if I /We change my/our address then it shall be my/our responsibility to inform the Company about it & get it changed in Company’s records, otherwise the communication sent to my/our given address shall be deemed to have been properly served on me/us.<br><br><br>
+(xiv) That I/We agree and acknowledge that the company, in its sole discretion, may abandon the project without assigning any reason thereof and in such an eventuality, the liability of the Company shall be limited only to refund me/us my/our invested amount.<br><br><br>
+(xv) That if any dispute arises at any time whatsoever, whether in relation to or in connection with this application form the same shall be adjudicated through arbitration, conducted by the arbitrator nominated exclusively by the Company. The venue of arbitration shall be in Delhi and procedure to be followed shall be decided by the arbitrator as and when need arises. It is clarified that such arbitrator may be associated with the Company. Arbitration shall be conducted in accordance with the Arbitrator and Conciliation Act, 1996. That the Courts at Delhi alone shall have the jurisdiction in all matters whatsoever arising at any time regardless of the place of execution of documents. The governing law will be the law of India.<br><br><br>
+(xvi) That no preference or choice will be given towards allotment of any particular unit. The size of unit is tentative & subject to change.<br><br><br>
+(xvii) Resale of the plot is not allowed for 12 months from date of booking.<br><br><br>
+(xviii) A transfer fee of 2 % of resale value shall be charged in case of resale.</p>
+              
+<h3 style="margin-bottom: 50px;">Signature of the Sole/First Applicant</h3>
+Place............................<br>
+Date.............................<br>
+Signature........................<br>
+Code.............................<br>
+Name.............................<br>
+Address..........................<br>
+Telephone no.....................<br>
+
+<h3 style="margin-bottom: 50px;"">Please enclose a copy of the following in respect of each applicant:-</h3>
+
+1. Permanent Account No.<br><br>
+2. Residential Proof<br><br>
+3. Photograph<br><br>
+4. Bank attested signature
+
+
+<p style="float: right;margin-top: 50px;">Note: - Please tick wherever applicable</p>
+   </div>
+       </div>
+       </div>
+       
+    
+
+</body>
+</html>
